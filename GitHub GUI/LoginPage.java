0c0ef36c3978af0db9330ad286d137f36500f9f9
Dhan Tamparong <111488321+su1kagg@users.noreply.github.com>
2023-03-09 12:59:26 +0800

    Update LoginPage.java

--- GitHub GUI/LoginPage.java
+++ GitHub GUI/LoginPage.java
@@ -6,7 +6,7 @@ public class LoginPage {
 
 	JFrame frame;
 	private JTextField txtUsernameOrEmail;
-	private JLabel lblNewLabel;
+	private JLabel lblTitleTextGIT;
 	private JPasswordField pwdPassword;
 
 	/**
@@ -37,8 +37,8 @@ public class LoginPage {
 	 * Initialize the contents of the frame.
 	 */
 	boolean passwordVisible = false;
-	private JLabel lblLoginMessage = new JLabel("");
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.getContentPane().setForeground(new Color(255, 255, 255));
 		frame.getContentPane().setBackground(new Color(9, 9, 9));
@@ -46,79 +46,53 @@ public class LoginPage {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		lblTitleTextGIT = new JLabel("Sign in to GitHub");
+		lblTitleTextGIT.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitleTextGIT.setBounds(186, 110, 200, 43);
+		lblTitleTextGIT.setForeground(new Color(255, 255, 255));
+		lblTitleTextGIT.setFont(new Font("Monospaced", Font.BOLD, 18));
+		frame.getContentPane().add(lblTitleTextGIT);
+
+	
+		
 		JLabel lblEmailorPhone = new JLabel("Username or Email");
-		lblEmailorPhone.setBounds(184, 164, 182, 14);
+		lblEmailorPhone.setBounds(184, 164, 202, 14);
 		lblEmailorPhone.setForeground(new Color(255, 255, 255));
-		lblEmailorPhone.setFont(new Font("Fira Code", Font.BOLD, 12));
+		lblEmailorPhone.setFont(new Font("Monospaced", Font.BOLD, 13));
 		frame.getContentPane().add(lblEmailorPhone);
 		
-		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(184, 210, 67, 14);
-		lblPassword.setForeground(new Color(255, 255, 255));
-		lblPassword.setFont(new Font("Fira Code", Font.BOLD, 12));
-		frame.getContentPane().add(lblPassword);
-		
 		txtUsernameOrEmail = new JTextField();
-		txtUsernameOrEmail.setFont(new Font("Fira Code", Font.PLAIN, 12));
-		txtUsernameOrEmail.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				if (txtUsernameOrEmail.getText().equals("Username or Email")) {
-					txtUsernameOrEmail.setText("");
-				} else {
-					txtUsernameOrEmail.selectAll();
-				}
-			}
-			@Override
-			public void focusLost(FocusEvent e) {
-				if(txtUsernameOrEmail.getText().equals(""))
-					txtUsernameOrEmail.setText("Username or Email");
-			}
-		});
-		txtUsernameOrEmail.setText("Username or Email");
+		txtUsernameOrEmail.setFont(new Font("Monospaced", Font.BOLD,12));
 		txtUsernameOrEmail.setBounds(184, 179, 200, 20);
 		txtUsernameOrEmail.setBackground(new Color(255, 255, 255));
 		frame.getContentPane().add(txtUsernameOrEmail);
 		txtUsernameOrEmail.setColumns(10);
 		
-		lblNewLabel = new JLabel("Sign in to GitHub");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(186, 110, 200, 43);
-		lblNewLabel.setForeground(new Color(255, 255, 255));
-		lblNewLabel.setFont(new Font("Fira Code", Font.BOLD, 15));
-		frame.getContentPane().add(lblNewLabel);
-		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(184, 210, 200, 14);
+		lblPassword.setForeground(new Color(255, 255, 255));
+		lblPassword.setFont(new Font("Monospaced", Font.BOLD, 12));
+		frame.getContentPane().add(lblPassword);
+
 		pwdPassword = new JPasswordField();
-		pwdPassword.setFont(new Font("Fira Code", Font.PLAIN, 12));
-		pwdPassword.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				if(pwdPassword.getText().equals("Password")) {
-					pwdPassword.setEchoChar('\u2022');
-					pwdPassword.setText("");
-				} else {
-					pwdPassword.selectAll();
-				}
-			}
-			@Override
-			public void focusLost(FocusEvent e) {
-				if(pwdPassword.getText().equals(""))
-					pwdPassword.setText("Password");
-					pwdPassword.setEchoChar((char)0);
-			}
-		});
-		pwdPassword.setText("Password");
-		pwdPassword.setEchoChar((char)0);
+		pwdPassword.setFont(new Font("Monospaced", Font.BOLD, 12));
 		pwdPassword.setBounds(184, 226, 182, 20);
 		pwdPassword.setBackground(new Color(255, 255, 255));
 		frame.getContentPane().add(pwdPassword);
 		
 		
-		JButton btnSingUP = new JButton("Sign Up");
-		btnSingUP.setFont(new Font("Fira Code", Font.PLAIN, 12));
-		btnSingUP.setBounds(294, 395, 90, 23);
-		btnSingUP.setBackground(new Color(192, 192, 192));
-		btnSingUP.addMouseListener(new MouseAdapter() {
+		JLabel lblNew2Git = new JLabel("New to GitHub?");
+		lblNew2Git.setFont(new Font("Monospaced", Font.BOLD, 12));
+		lblNew2Git.setBounds(186, 372, 109, 20);
+		lblNew2Git.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNew2Git.setForeground(Color.LIGHT_GRAY);
+		frame.getContentPane().add(lblNew2Git);
+		
+		JButton btnSignUp = new JButton("Sign Up");
+		btnSignUp.setFont(new Font("Monospaced", Font.BOLD, 12));
+		btnSignUp.setBounds(295, 372, 91, 20);
+		btnSignUp.setBackground(new Color(192, 192, 192));
+		btnSignUp.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				SignUp window = new SignUp();
@@ -126,14 +100,8 @@ public class LoginPage {
 				frame.dispose();
 			}
 		});
-		frame.getContentPane().add(btnSingUP);
+		frame.getContentPane().add(btnSignUp);
 		
-		JLabel lblNewLabel_1 = new JLabel("New to GitHub?");
-		lblNewLabel_1.setFont(new Font("Fira Code", Font.PLAIN, 12));
-		lblNewLabel_1.setBounds(184, 398, 119, 17);
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setForeground(Color.LIGHT_GRAY);
-		frame.getContentPane().add(lblNewLabel_1);
 	
 		
 		JRadioButton rdbtnShowPass = new JRadioButton("");
@@ -150,24 +118,40 @@ public class LoginPage {
 		frame.getContentPane().add(rdbtnShowPass);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Remember Me");
-		chckbxNewCheckBox.setFont(new Font("Fira Code", Font.PLAIN, 12));
+		chckbxNewCheckBox.setVerticalAlignment(SwingConstants.TOP);
+		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.LEFT);
+		chckbxNewCheckBox.setFont(new Font("Monospaced", Font.BOLD, 13));
 		chckbxNewCheckBox.setBackground(new Color(9, 9, 9));
 		chckbxNewCheckBox.setForeground(new Color(255, 255, 255));
-		chckbxNewCheckBox.setBounds(184, 248, 200, 23);
+		chckbxNewCheckBox.setBounds(186, 248, 198, 20);
 		frame.getContentPane().add(chckbxNewCheckBox);
 		
-		lblLoginMessage.setForeground(Color.WHITE);
-		lblLoginMessage.setFont(new Font("Arial", Font.PLAIN, 12));
-		lblLoginMessage.setBounds(184, 278, 200, 14);
-		frame.getContentPane().add(lblLoginMessage);
 		
-		JPanel pnlBtnLogin = new JPanel();
-		pnlBtnLogin.addMouseListener(new MouseAdapter() {
+		JButton btnForgotPass = new JButton("Forgot Password?");
+		btnForgotPass.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ForgotPass window = new ForgotPass();
+				window.frame.setVisible(true);
+			}
+		});
+		btnForgotPass.setFont(new Font("Monospaced", Font.BOLD, 12));
+		btnForgotPass.setForeground(new Color(255, 255, 255));
+		btnForgotPass.setBorder(null);
+		btnForgotPass.setBackground(new Color(9, 9, 9));
+		btnForgotPass.setBounds(186, 330, 200, 25);
+		frame.getContentPane().add(btnForgotPass);
+		
+		JButton btnLoginBTN = new JButton("Login");
+		btnLoginBTN.setFont(new Font("Monospaced", Font.BOLD, 12));
+		btnLoginBTN.setForeground(new Color(255, 255, 255));
+		btnLoginBTN.setBackground(new Color(0, 166, 0));
+		btnLoginBTN.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				//if user inputs are correct
 				if(txtUsernameOrEmail.getText().equals("DhanAngelo") && pwdPassword.getText().equals("BasilioTamparong123")) {
-					lblLoginMessage.setText("");
+					
 					JOptionPane.showMessageDialog(null, "Login Successful! \nWelcome to GitHub");
 	
 					HomeScreenGitHub window2 = new HomeScreenGitHub();
@@ -175,40 +159,15 @@ public class LoginPage {
 					frame.dispose();
 				}
 				//checks if the user didn't fill all fields
-				else if (txtUsernameOrEmail.getText().equals("") || txtUsernameOrEmail.getText().equals("Username") ||
-						pwdPassword.getText().equals("") || pwdPassword.getText().equals("Password")) {
-					lblLoginMessage.setText("Please input all requirements!");
+				else if (txtUsernameOrEmail.getText().equals("") || pwdPassword.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Please input all requirements!");
 				} else {
-					lblLoginMessage.setText("Username and Password didn't match!");		//checks if the user's input are incorrect.
+					JOptionPane.showMessageDialog(null,"Username and Password didn't match!");
 				}
 			}
 		});
-		pnlBtnLogin.setBackground(new Color(0, 166, 0));
-		pnlBtnLogin.setBounds(184, 300, 200, 28);
-		frame.getContentPane().add(pnlBtnLogin);
-		pnlBtnLogin.setLayout(null);
-		
-		JLabel lblSignIn = new JLabel("Sign in");
-		lblSignIn.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSignIn.setFont(new Font("Fira Code", Font.BOLD, 12));
-		lblSignIn.setForeground(new Color(255, 255, 255));
-		lblSignIn.setBounds(0, 0, 200, 28);
-		pnlBtnLogin.add(lblSignIn);
-		
-		JButton btnNewButton = new JButton("Forgot Password?");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				ForgotPass window = new ForgotPass();
-				window.frame.setVisible(true);
-			}
-		});
-		btnNewButton.setFont(new Font("Fira Code", Font.PLAIN, 12));
-		btnNewButton.setForeground(new Color(255, 255, 255));
-		btnNewButton.setBorder(null);
-		btnNewButton.setBackground(new Color(9, 9, 9));
-		btnNewButton.setBounds(225, 328, 119, 23);
-		frame.getContentPane().add(btnNewButton);
+		btnLoginBTN.setBounds(188, 291, 200, 28);
+		frame.getContentPane().add(btnLoginBTN);
 
 	}
 }
